4df0a60fcb15b9da1d87a172710aefc631b4d0c1
VladimirAnic <vladimiranic@gmail.com>
2017-04-10 21:53:22 +0200

    layout almost complete, database needed to continue

--- app/src/main/java/com/example/vladimir/dz3/NewTask.java
+++ app/src/main/java/com/example/vladimir/dz3/NewTask.java
@@ -2,12 +2,38 @@ package com.example.vladimir.dz3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class NewTask extends Activity {
+
+    Spinner spPriority, spCategory;
+    EditText etNewTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+        setUpUI();
+    }
+
+    private void setUpUI() {
+        this.spPriority = (Spinner) findViewById(R.id.spPriority);
+        this.spCategory = (Spinner) findViewById(R.id.spCategory);
+        this.etNewTask = (EditText) findViewById(R.id.etNewTask);
+
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter
+                .createFromResource(this, R.array.priority,
+                        android.R.layout.simple_spinner_item);
+
+        staticAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spPriority.setAdapter(staticAdapter);
+
+
+        staticAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spCategory.setAdapter(staticAdapter);
     }
 }
